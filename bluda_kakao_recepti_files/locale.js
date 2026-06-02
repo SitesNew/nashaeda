955832ec6ddbc7e9ad362b891a18e5ec4a2b1b6c
Valentin1950 <pvipvi@i.ua>
2026-06-02 15:41:10 +0300

    My Site nashaeda

---
+++ bluda_kakao_recepti_files/locale.js
@@ -0,0 +1,29 @@
+/*
+ * jQuery File Upload Plugin Localization Example 6.5.1
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2012, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*global window */
+
+window.locale = {
+    "fileupload": {
+        "errors": {
+            "maxFileSize": "Р¤Р°Р№Р» СЃР»РёС€РєРѕРј Р±РѕР»СЊС€РѕР№",
+            "minFileSize": "Р¤Р°Р№Р» СЃР»РёС€РєРѕРј РјР°Р»РµРЅСЊРєРёР№",
+            "acceptFileTypes": "Р—Р°РїСЂРµС‰РµРЅРЅС‹Р№ С‚РёРї С„Р°Р№Р»Р°",
+            "maxNumberOfFiles": "Р”РѕСЃС‚РёРіРЅСѓС‚Рѕ РјР°РєСЃРёРјР°Р»СЊРЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ С„Р°Р№Р»РѕРІ",
+            "uploadedBytes": "Uploaded bytes exceed file size",
+            "emptyResult": "Empty file upload result"
+        },
+        "error": "РћС€РёР±РєР°",
+        "start": "Р—Р°РіСЂСѓР·РёС‚СЊ",
+        "cancel": "РћС‚РјРµРЅР°",
+        "destroy": "РЈРґР°Р»РёС‚СЊ"
+    }
+};
